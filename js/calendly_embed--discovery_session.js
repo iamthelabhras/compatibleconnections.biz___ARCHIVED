942c1e8abddf8de2e35f00c3e33ed46d38bc7fe2
Labhrás Mac Gabhann <coding@larrymg.me>
2021-07-19 21:57:00 -0400

    Implemented Sass.

--- js/calendly_embed--discovery_session.js
+++
@@ -1,6 +0,0 @@
-Calendly.initInlineWidget({
-    url: 'https://calendly.com/ccbiz',
-    parentElement: document.getElementById('calendly_embed--discovery_session'),
-    prefill: {},
-    utm: {}
-   });
