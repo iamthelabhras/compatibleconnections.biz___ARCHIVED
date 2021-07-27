26b0ce1b6a5e8de3cead716c0e01a7be56048b4b
Labhrás Mac Gabhann <coding@larrymg.me>
2021-07-27 02:00:13 -0400

    feat(php): Added confirmation message & delayed redirect.

--- src/resources/dating_tips_suggestions.php
+++ src/resources/dating_tips_suggestions.php
@@ -15,4 +15,12 @@ if (isset($_POST['suggestion__dating_tip']) && $_POST['suggestion__dating_tip'] 
 
 	// reset $message_sent Boolean
 	$message_sent = true;
+	//	TO DO:	Utilize this Boolean value to trigger actions when mail is successful sent.
+
+	echo 	"
+		<h1>Thank you for sharing your Dating Tip suggestion with us!</h1>
+		<p>Your browser will be directed back to the Dating Tips page in a few moments.</p>	
+		";
+
+	header("Refresh:3; url=https://compatibleconnections.biz/resources/dating_tips.html", true, 303);
 }
